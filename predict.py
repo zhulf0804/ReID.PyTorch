0d6367bbb06e90a3e3dcbdb589b91f201804a3c7
zhulf <zhulf0804@gmail.com>
2019-11-07 12:38:53 +0800

    fix predict

--- predict.py
+++ predict.py
@@ -2,28 +2,38 @@ import json
 import argparse
 import torch.nn as nn
 import torch
+import numpy as np
+import os
 from models.resnet import resnet50
+from models.densenet import densenet121
 from datasets import get_test_datasets
 from utils.eval_utils import extract_features, get_filenames, inference
 
-reid_dir = '/Users/zhulf/data/reid_match/reid'
-example_file = '/Users/zhulf/data/reid_match/raw/submission_example_A.json'
+reid_dir = '/root/data/reid'
+example_file = '/root/data/reid/submission_example_A.json'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type=str, default=reid_dir, help='Dataset directory')
 parser.add_argument('--batch_size', type=int, default=32, help='Batch size')
-parser.add_argument('--num_classes', type=int, default=751, help='Batch size')
+parser.add_argument('--num_classes', type=int, default=4768, help='Num classes to load the raw model')
 parser.add_argument('--checkpoint', type=str, default='', help='Directory to save checkpoints')
+parser.add_argument('--model', type=str, default='resnet50', help='Model to use')
+parser.add_argument('--saved_dir', type=str, default='results', help='Saved directory name')
+parser.add_argument('--saved_filename', type=str, help='Saved filename')
 args = parser.parse_args()
 
 
 test_image_datasets, test_dataloaders, dataset_sizes, class_names = get_test_datasets(args.data_dir, args.batch_size)
 num_classes = len(class_names)
-model = resnet50(num_classes=args.num_classes)
+if args.model == 'resnet50':
+    model = resnet50(num_classes=args.num_classes)
+elif args.model == 'densenet121':
+    model = densenet121(num_classes=args.num_classes)
 model.load_state_dict(torch.load(args.checkpoint))
 model.classifier.classifier = nn.Sequential()
 print(model)
-
+if not os.path.exists(args.saved_dir):
+    os.makedirs(args.saved_dir)
 
 def predict(model):
     print("=" * 20, "Extracting features from query and gallery ...", "=" * 20)
@@ -37,11 +47,11 @@ def predict(model):
     gallery_filenames = get_filenames(gallery_paths)
 
     print("=" * 20, "Predicting ...", "=" * 20)
-    saved_json = '/root/data/reid/first_result.json'
+    saved_json = os.path.join(args.saved_dir, args.saved_filename + '.json')
     d = {}
     for i in range(len(query_filenames)):
         index = inference(query_features[i], gallery_features, top=200)
-        d[query_filenames[i].strip()] = gallery_filenames[index].tolist()
+        d[query_filenames[i].strip()] = np.array(gallery_filenames)[index].tolist()
     
     with open(example_file, 'r') as f:
         examples = json.loads(f.read())
@@ -57,3 +67,6 @@ def predict(model):
 
 if __name__ == '__main__':
     predict(model)
+
+
+# python predict.py --model  --checkpoint  --saved_filename
